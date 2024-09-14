2c7a92079f00783f9ef81345393324773cd8d295
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-14 07:08:11 +0100

    updated

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -5,7 +5,7 @@
             @csrf
             <div class="mb-4">
                 <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Role Name</label>
-                <input type="text" id="name" value="{{ old('name') ?? $role->name}}" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                <input type="text" id="name" value="{{ old('name') }}" name="name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
             </div>
             <div class="flex items-center justify-between">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -13,7 +13,7 @@
                 <input type="email" id="email" name="email" value="{{ old('email', $user->email) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
             </div>
             <div class="mb-4">
-                <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Role</label>
+                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Role</label>
                 <div class="mb-4">
                     <input type="text" id="name" name="name" value="{{ old('name', $user->role) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                 </div>
@@ -25,33 +25,5 @@
             </div>
         </form>
     </div>
-    {{-- <div class="container mx-auto py-8">
-        <h1 class="text-2xl font-semibold mb-6">Edit User</h1>
-        <form action="{{ route('users.update', $user->id) }}" method="POST" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            @csrf
-            @method('PUT')
-            <div class="mb-4">
-                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Name</label>
-                <input type="text" id="name" name="name" value="{{ $user->name }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-            </div>
-            <div class="mb-4">
-                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
-                <input type="email" id="email" name="email" value="{{ $user->email }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-            </div>
-            <div class="mb-4">
-                <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Role</label>
-                <select id="role" name="role" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
-                    <option value="admin" {{ $user->role == 'admin' ? 'selected' : '' }}>Admin</option>
-                    <option value="user" {{ $user->role == 'user' ? 'selected' : '' }}>User</option>
-                    <option value="project manager" {{ $user->role == 'Project Manager' ? 'selected' : '' }}>Project Manager</option>
-                    <option value="editor" {{ $user->role == 'Editor' ? 'selected' : '' }}>Editor</option>
-                </select>
-            </div>
-            <div class="flex items-center justify-between">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    Update User
-                </button>
-            </div>
-        </form>
-    </div> --}}
+    
 </x-app-layout>
