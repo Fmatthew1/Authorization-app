dc0c4665c744985e5f50b0f984f012964900eb2c
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-18 14:52:13 +0100

    properly indented

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,59 +7,45 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-   // Display list of users excluding existing admins
+         // Display list of users excluding existing admins
 
-     // Show users who are not admins
-        public function showUsers()
-        {
-         // Get users who do not have the 'admin' role
+         // Show users who are not admins
+         public function showUsers()
+         {
+            // Get users who do not have the 'admin' role
             $users = User::whereDoesntHave('roles', function ($query) {
                $query->where('name', 'admin');
             })->get();
 
             return view('admin.manage-users', compact('users'));
-          // Get the admin role
-      //   $adminRole = Role::where('name', 'admin')->first();
-      //   // Retrieve users whose role is not admin
-      //   $users = User::where('role_id', '!=', $adminRole->id)->get();
-      //    //$users = User::where('role', '!=', 'admin')->get(); // Fetch non-admin users
-      //    return view('admin.manage-users', compact('users'));
-        }
+            
+         }
 
-     // Make a user an admin
-        public function makeAdmin($id)
-        {
-         // Get the 'admin' role
-         $adminRole = Role::where('name', 'admin')->first();
+         // Make a user an admin
+         public function makeAdmin($id)
+         {
+            // Get the 'admin' role
+            $adminRole = Role::where('name', 'admin')->first();
 
-         if (!$adminRole) {
-        // Handle if 'admin' role does not exist
-        return redirect()->route('admin.manageUsers')->with('error', 'Admin role not found.');
-    }
+            if (!$adminRole) {
+         // Handle if 'admin' role does not exist
+            return redirect()->route('admin.manageUsers')->with('error', 'Admin role not found.');
+      }
 
-    // Find the user by ID
-         $user = User::find($id);
+         // Find the user by ID
+            $user = User::find($id);
 
-      if ($user) {
-        // Attach the 'admin' role to the user
-        $user->roles()->attach($adminRole->id);
+            if ($user) {
+            // Attach the 'admin' role to the user
+            $user->roles()->attach($adminRole->id);
 
-        return redirect()->route('admin.manageUsers')->with('success', 'User has been made an admin.');
-    }
+            return redirect()->route('admin.manageUsers')->with('success', 'User has been made an admin.');
+      }
 
-    // If user is not found
-      return redirect()->route('admin.manageUsers')->with('error', 'User not found.');
-      //   $user = User::find($id);
-
-      //   if ($user) {
-      //       $user->role_id = 1; // Update user role to admin
-      //       $user->save();
-
-      //       return redirect()->route('admin.manageUsers')->with('success', 'User has been made an admin.');
-      //   }
-
-      //   return redirect()->route('admin.manageUsers')->with('error', 'User not found.');
-        }
+         // If user is not found
+            return redirect()->route('admin.manageUsers')->with('error', 'User not found.');
+         
+            }
 
   
 }
