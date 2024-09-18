731837e68efa4deebcffe7458750596620db4489
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-18 12:47:25 +0100

    removed the role field

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,12 +12,12 @@
                 <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
                 <input type="email" id="email" name="email" value="{{ old('email', $user->email) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
             </div>
-            <div class="mb-4">
+            {{-- <div class="mb-4">
                 <label for="role" class="block text-gray-700 text-sm font-bold mb-2">Role</label>
                 <div class="mb-4">
                     <input type="text" id="role" name="role" value="{{ old('name', $user->role) }}" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
                 </div>
-            </div>
+            </div> --}}
             <div class="flex items-center justify-between">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                     Update User
