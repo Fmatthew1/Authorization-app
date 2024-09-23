2820143a6a9defac14da2e1e676bbe485ae67d48
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-23 20:36:45 +0100

    edited the css

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,15 +6,15 @@
         <table class="min-w-full bg-white shadow-md rounded">
             <thead>
                 <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                    <th class="py-3 px-6 text-left">#</th>
-                    <th class="py-3 px-6 text-left">Name</th>
-                    <th class="py-3 px-6 text-left">Description</th>
-                    <th class="py-3 px-6 text-left">Price</th>
-                    <th class="py-3 px-6 text-center">Quantity</th>
-                    <th class="py-3 px-6 text-center">Created By</th>
-                    <th class="py-3 px-6 text-center">Confirmed By</th>
-                    <th class="py-3 px-6 text-center">Status</th>
-                    <th class="py-3 px-6 text-center">Actions</th>
+                    <th class="py-2 px-3 text-left">#</th>
+                    <th class="py-2 px-3 text-left">Name</th>
+                    <th class="py-2 px-3 text-left">Description</th>
+                    <th class="py-2 px-3 text-left">Price</th>
+                    <th class="py-2 px-3 text-center">Quantity</th>
+                    <th class="py-2 px-3 text-center">Created By</th>
+                    <th class="py-2 px-3 text-center">Confirmed By</th>
+                    <th class="py-2 px-3 text-center">Status</th>
+                    <th class="py-2 px-3 text-center">Actions</th>
                 </tr>
             </thead>
             <tbody class="text-gray-600 text-sm font-light">
@@ -28,11 +28,12 @@
                         <td class="py-3 px-6 text-left">{{ $product->created_by }}</td>
                         <td class="py-3 px-6 text-left">{{ $product->confirmed_by }}</td>
                         <td class="py-3 px-6 text-left">{{ $product->status->name }}</td>
-                        <td class="py-3 px-6 text-center">
+                        <div class="flex item-center justify-center">
+                        <td class="py-2 px-4 text-center">
                             @can('update', $product)
                                 <form action="{{ route('products.index', $product->id) }}" method="POST" class="inline">
                                     @csrf
-                                    <button type="submit" class="bg-gray-500 text-white px-4 py-2 rounded">
+                                    <button type="submit" class="inline-flex bg-gray-500 text-white px-3 py-2 rounded">
                                         Forwarded
                                     </button>
                                 </form>
@@ -41,17 +42,17 @@
                             @can('confirm', $product)
                                 <form action="{{ route('products.index', $product->id) }}" method="POST" class="inline">
                                     @csrf
-                                    <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded">
+                                    <button type="submit" class="inline-flex bg-green-500 text-white px-3 py-2 rounded">
                                         Confirmed
                                     </button>
                                 </form>
                             @endcan
-                            <div class="flex item-center justify-center">
-                                <a href="{{ route('products.show', $product->id) }}" class="bg-blue-500 px-4 py-2 text-white rounded">
+                            
+                                <a href="{{ route('products.show', $product->id) }}" class="inline-flex bg-blue-500 px-3 py-2 text-white rounded">
                                     View
                                 </a>
                                 @can('update', $product)
-                                <a href="{{ route('products.edit', $product->id) }}" class="bg-yellow-500 ml-2 px-4 py-2 text-white rounded">
+                                <a href="{{ route('products.edit', $product->id) }}" class="inline-flex bg-yellow-500 ml-2 px-3 py-2 text-white rounded">
                                     Edit
                                 </a>
                                 @endcan
@@ -59,7 +60,7 @@
                                 <form action="{{ route('products.destroy', $product->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="bg-red-500 ml-2 px-4 py-2 text-white rounded">
+                                    <button type="submit" class="inline-flex bg-red-500 ml-2 px-3 py-2 text-white rounded">
                                         Delete
                                     </button>
                                 </form>
