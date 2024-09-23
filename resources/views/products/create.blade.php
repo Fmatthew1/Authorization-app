558014e0386c2e8d6de0cd292337abdf09b2c71c
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-23 20:16:34 +0100

    added created_by and confirmed_by column

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -36,6 +36,10 @@
                                 <label for="quantity" class="">Quantity</label>
                                 <input id="quantity" type="number" class=" w-full @error('quantity') is-invalid @enderror" name="quantity" value="{{ old('quantity') }}">
                             </div>
+                            <!-- Hidden fields for created_by and confirmed_by -->
+                                <input type="hidden" name="created_by" value="{{ auth()->user()->id }}">
+                                <input type="hidden" name="confirmed_by" value="">
+
                             <button type="submit" class="bg-green-600 px-6 py-2 mt-4 w-1/2 text-white text-xl rounded">
                                 Submit
                             </button>
