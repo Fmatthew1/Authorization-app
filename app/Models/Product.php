f9efd9a6d7c274ea1754e9d78ef4b7e1d9114409
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-23 20:10:56 +0100

    updated the $fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -14,5 +15,28 @@ class Product extends Model
         'description',
         'price',
         'quantity',
+        'created_by', 
+        'confirmed_by',
+        'status_id',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            // Set default status as Pending (assuming Pending has ID 1)
+            $product->status_id = 1; // Pending status ID from the statuses table
+        });
+    }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    
+
+
+
 }
