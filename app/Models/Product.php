57137f9bb08f45f46e21da16658da9655d031400
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-25 10:24:37 +0100

    updated

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,7 +18,24 @@ class Product extends Model
         'created_by', 
         'confirmed_by',
         'status_id',
+        'creator_id',
+        'project_manager_id',
     ];
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function projectManager()
+    {
+        return $this->belongsTo(User::class, 'project_manager_id');
+    }
 
     public static function boot()
     {
@@ -30,13 +47,14 @@ class Product extends Model
         });
     }
 
-    public function status(): BelongsTo
+    public function forward()
     {
-        return $this->belongsTo(Status::class);
+
+        $this->status_id === 2;
+        // if ($this->status_id == 1) { // Assuming 1 is for 'Pending'
+        //     $this->status_id = 2; // Assuming 2 is for 'Forwarded'
+             $this->save();
+        // }
     }
-
-    
-
-
 
 }
