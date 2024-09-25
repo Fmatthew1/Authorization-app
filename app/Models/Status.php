642841ea7a1f9f64b3ec1544f6ee2ba24edfc249
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-25 10:25:43 +0100

    added statuses variable

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -11,6 +11,23 @@ class Status extends Model
 
     protected $fillable = ['name'];
 
+    protected $table = 'statuses';
+
+    static $values = [
+        ['id' => 1, 'name' => 'Pending', 'enabled' => true], 
+        ['id' => 2, 'name' => 'Forwarded', 'enabled' => true], 
+        ['id' => 3, 'name' => 'Confirmed', 'enabled' => true], 
+
+    ];
+
+    protected $guarded = [];
+
+    public const PENDING                = 1;
+    public const FORWARDED              = 2;
+    public const CONFIRMED               = 3;
+
+
+
     public function products()
     {
         return $this->belongsToMany(Product::class);
