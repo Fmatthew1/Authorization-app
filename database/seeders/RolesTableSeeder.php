55bd8f25a01722a538cb5ebfd64eaba984e00266
Matthew Fekumoh <mattez2009@gmail.com>
2024-09-26 09:22:56 +0100

    no changes made

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 
 class RolesTableSeeder extends Seeder
@@ -19,4 +20,7 @@ class RolesTableSeeder extends Seeder
         Role::create(['name' => 'Project Manager']);
         Role::create(['name' => 'editor']);
     }
+
+//     
+
 }
