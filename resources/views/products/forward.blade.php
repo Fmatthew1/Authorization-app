9ca217261ddaffdff252416607a075572d285fb4
Matthew Fekumoh <mattez2009@gmail.com>
2024-10-01 13:45:57 +0100

    forward button hidden

--- resources/views/products/forward.blade.php
+++ resources/views/products/forward.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
 
     <h2>Forward Product: {{ $product->name }}</h2>
+    @if($product->status_id == 1)
     <form method="POST" action="{{ route('products.forward', $product->id) }}">
         @csrf
         <button type="submit" class="inline-flex bg-blue-500 ml-2 px-3 py-2 text-white rounded">Forward Product</button>
     </form>
-
+    @endif
 </x-app-layout>
